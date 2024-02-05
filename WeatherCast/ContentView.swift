7cf1db38cfc9eb616eac2948ab1c17c9210a1f4b
BasmaMohamedFathy <basma.m.fathy@gmail.com>
2024-02-05 18:21:26 +0200

    Initial Commit

---
+++ WeatherCast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherCast
+//
+//  Created by Basma on 05/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherCast/WeatherCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCastApp.swift
+//  WeatherCast
+//
+//  Created by Basma on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
